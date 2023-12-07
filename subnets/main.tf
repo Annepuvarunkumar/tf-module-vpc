dae8f6931947213d50973e14adf627795ecd37d6
varun <varunannepu123@gmail.com>
2023-12-07 23:35:13 +0530

    vpc

---
+++ subnets/main.tf
@@ -0,0 +1,17 @@
+#Here we are using the another subnet because where we are dealing with an map and our input is another map[ex: subnets]
+#then we will create the sub-module this is an general practice that will follow.
+
+resource "aws_subnet" "main" {
+  for_each            = var.subnets
+  vpc_id              = var.vpc_id
+  cidr_block          = each.value["cidr"]
+  availability_zone   = each.value["az"]
+}
+
+#we need the cidr and az information so we are looping in to for_each = var.subnets so that we can get cidr and a info
+#to create subnet.
+
+
+variable "subnets" {}
+variable "vpc_id" {}
+
