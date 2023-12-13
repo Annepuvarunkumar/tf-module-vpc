74dbbe333d5d8639cf354208c9feab49bca986ee
varun <varunannepu123@gmail.com>
2023-12-13 19:25:59 +0530

    vpc

--- locals.tf
+++ locals.tf
@@ -13,7 +13,7 @@ locals {
   public_route_table_ids    = [for k,v in lookup(lookup(module.subnets, "public", null), "route_table_ids", null): v.id]
   app_route_table_ids       = [for k,v in lookup(lookup(module.subnets, "app", null), "route_table_ids", null): v.id]
   db_route_table_ids        = [for k,v in lookup(lookup(module.subnets, "db", null), "route_table_ids", null): v.id]
-  private_route_table_ids   = concat (local.app_subnet_ids, local.db_subnet_ids)
+  private_route_table_ids   = concat (local.app_route_table_ids, local.db_route_table_ids)
 
 }
 
