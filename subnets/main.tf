a3def6021a9d66b61a5e2c789a16754f04cad864
varun <varunannepu123@gmail.com>
2023-12-16 10:37:43 +0530

    vpc

--- subnets/main.tf
+++ subnets/main.tf
@@ -7,20 +7,15 @@ resource "aws_subnet" "main" {
   vpc_id              = var.vpc_id
   cidr_block          = each.value["cidr"]
   availability_zone   = each.value["az"]
-
-  tags = {
-    Name = each.key
-  }
-}
+  tags                = merge(var.tags, { Name = "${var.env}-${each.key}-subnet}"})
+   }
 #we need the cidr and az information so we are looping in to for_each = var.subnets so that we can get cidr and a info
 #to create subnet.
 
 resource "aws_route_table" "main" {
   for_each  = var.subnets
   vpc_id    = var.vpc_id
-  tags = {
-    Name = each.key
-  }
+  tags      = merge(var.tags, { Name = "${var.env}-${each.key}-rt}"})
 }
 
 resource "aws_route_table_association" "a" {
--- subnets/vars.tf
+++ subnets/vars.tf
@@ -1,5 +1,7 @@
 variable "subnets" {}
 variable "vpc_id" {}
+variable "tags" {}
+variable "env" {}
 
 #here we have been created the subnets and vpc_id vars bcuz we have been used these variables in the resources
 #like subnet creation resource and route table creation resource.
